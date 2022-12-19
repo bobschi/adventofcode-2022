524b7e1fecb19536fe4d0c8a29a64da29090864d
Florian Holzner <florian@holzner.tech>
2022-12-20 00:14:06 +0100

    Added dictionary of possible outcomes.

--- day02.py
+++ day02.py
@@ -11,3 +11,22 @@ class Result(Enum):
     LOSS = 0
     DRAW = 3
     WIN = 6
+
+
+OUTCOMES: dict[Sign, dict[Sign, Result]] = {
+    Sign.ROCK: {
+        Sign.ROCK: Result.DRAW,
+        Sign.PAPER: Result.WIN,
+        Sign.SCISSORS: Result.LOSS,
+    },
+    Sign.PAPER: {
+        Sign.ROCK: Result.LOSS,
+        Sign.PAPER: Result.DRAW,
+        Sign.SCISSORS: Result.WIN,
+    },
+    Sign.SCISSORS: {
+        Sign.ROCK: Result.WIN,
+        Sign.PAPER: Result.LOSS,
+        Sign.SCISSORS: Result.DRAW,
+    },
+}

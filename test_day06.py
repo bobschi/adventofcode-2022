82c67924ae4df847de5dd78366b593b4337ca8ad
Florian «bobschi» Holzner <accounts@lebobs.ch>
2022-12-21 00:10:22 +0100

    Tested partially applied functions.

--- test_day06.py
+++ test_day06.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day06 import find_marker
+from day06 import find_start_of_packet_marker, find_start_of_message_marker
 
 
 @pytest.mark.parametrize(
@@ -14,6 +14,22 @@ from day06 import find_marker
     ),
 )
 def test_find_marker(datastream: str, expected_index: int):
-    index = find_marker(4, datastream)
+    index = find_start_of_packet_marker(datastream)
+
+    assert index == expected_index
+
+
+@pytest.mark.parametrize(
+    "datastream,expected_index",
+    (
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ),
+)
+def test_find_start_of_message_marker(datastream: str, expected_index: int):
+    index = find_start_of_message_marker(datastream)
 
     assert index == expected_index

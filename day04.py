66f5cfcf983578cc2f51b2c26b48acd90b68a875
Florian «bobschi» Holzner <accounts@lebobs.ch>
2022-12-20 11:59:28 +0100

    Implemented checking for total overlap.

--- day04.py
+++ day04.py
@@ -2,4 +2,4 @@ def do_assigments_fully_overlap(assignment_a: set[int], assignment_b: set[int]) 
     """
     Return True if one assigment is fully contained in the other, False if not.
     """
-    ...
+    return assignment_a <= assignment_b or assignment_b <= assignment_a

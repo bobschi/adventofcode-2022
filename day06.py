41ef08850cc2fac52f503f69e862f17a37f8a144
Florian «bobschi» Holzner <accounts@lebobs.ch>
2022-12-20 23:55:39 +0100

    Implemeted finding the start of packet marker.

--- day06.py
+++ day06.py
@@ -1,2 +1,10 @@
 def find_first_marker(datastream: str) -> int:
-    ...
+    """
+    Return the last character index of the first group of four distinct characters.
+    """
+    for i in range(len(datastream) - 3):
+        current_sequence = set(datastream[i : i + 4])
+        if len(current_sequence) == 4:
+            return i + 4
+
+    return -1

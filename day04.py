29c4db5ed4fd31b4a97d96147ace9bbaf6852845
Florian «bobschi» Holzner <accounts@lebobs.ch>
2022-12-20 12:01:45 +0100

    Added a dataclass for elves' assignments.

--- day04.py
+++ day04.py
@@ -1,5 +1,17 @@
+from dataclasses import dataclass
+
+
 def do_assigments_fully_overlap(assignment_a: set[int], assignment_b: set[int]) -> bool:
     """
     Return True if one assigment is fully contained in the other, False if not.
     """
     return assignment_a <= assignment_b or assignment_b <= assignment_a
+
+
+@dataclass
+class Elf:
+    assignment_start: int
+    assignment_end: int
+
+    def get_assignment(self) -> set[int]:
+        return set(range(self.assignment_start, self.assignment_end))

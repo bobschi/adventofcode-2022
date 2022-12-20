0b90a9a6c3b85161818c83b7dfb153a0a327892e
Florian «bobschi» Holzner <accounts@lebobs.ch>
2022-12-20 23:47:33 +0100

    Started work on day six. Added first test.

---
+++ test_day06.py
@@ -0,0 +1,19 @@
+import pytest
+
+from day06 import find_first_marker
+
+
+@pytest.mark.parametrize(
+    "datastream,expected_index",
+    (
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    ),
+)
+def test_find_first_marker(datastream: str, expected_index: int):
+    index = find_first_marker(datastream)
+
+    assert index == expected_index

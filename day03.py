d88b1b80293671e541140d8b9e1a356616b6b42d
Florian Holzner <florian@holzner.tech>
2022-12-20 01:51:03 +0100

    Implemented rucksack packing.

--- day03.py
+++ day03.py
@@ -7,5 +7,9 @@ class Rucksack:
     compartment_two: str
 
 
-def pack_into_rucksack(contents) -> Rucksack:
-    ...
+def pack_into_rucksack(contents: str) -> Rucksack:
+    length = len(contents)
+    return Rucksack(
+        compartment_one=contents[: length // 2],
+        compartment_two=contents[length // 2 :],
+    )

1de201ff3fe57eee40b541dba66e286554ce7478
Florian Holzner <florian@holzner.tech>
2022-12-24 23:52:18 +0100

    Day 8: Restarting solution.
    I misread how trees are counted. Pretty obvious mistake.

--- day08.py
+++ day08.py
@@ -1,42 +1,11 @@
-import enum
 from pathlib import Path
-
-
-class Direction(enum.Enum):
-    UP = enum.auto()
-    RIGHT = enum.auto()
-    DOWN = enum.auto()
-    LEFT = enum.auto()
 
 
 Map = list[list[int]]
 
 
-def visible_trees_in_line(map: Map, direction: Direction, line: int) -> int:
-    if direction in [Direction.UP, Direction.DOWN]:
-        line_of_trees = [row[line] for row in map]
-
-    if direction in [Direction.LEFT, Direction.RIGHT]:
-        line_of_trees = map[line]
-
-    if direction in [Direction.UP, Direction.RIGHT]:
-        line_of_trees.reverse()
-
-    def are_neighbors_smaller(trees: list[int], current_position: int) -> bool:
-        for tree in trees[:current_position]:
-            if tree >= trees[current_position]:
-                return False
-
-        return True
-
-    count = 0
-    for spot, height in enumerate(line_of_trees):
-        if height == 0 or not are_neighbors_smaller(line_of_trees, spot):
-            continue
-
-        count = count + 1
-
-    return count
+def visible_trees_from_outside(map: Map) -> int:
+    ...
 
 
 def read_map(path_to_map: Path) -> Map:
@@ -44,3 +13,10 @@ def read_map(path_to_map: Path) -> Map:
         map_contents = map_file.readlines()
 
     return [[int(height) for height in list(line.strip())] for line in map_contents]
+
+
+def solve_part_one() -> None:
+    map = read_map("inputs/day08_input.py")
+    visible_trees = visible_trees_from_outside(map)
+
+    print(f"The solution to part one is {visible_trees}")
--- test_day08.py
+++ test_day08.py
@@ -1,25 +1,11 @@
 from functools import partial
 import pytest
 
-from day08 import Direction, Map, read_map, visible_trees_in_line
-
-
-@pytest.fixture
-def asymmetric() -> Map:
-    return [
-        [0, 0, 0, 1],
-        [0, 1, 0, 1],
-        [0, 0, 0, 1],
-    ]
-
-
-@pytest.fixture
-def ring_of_trees() -> Map:
-    return [
-        [1, 2, 1],
-        [2, 0, 2],
-        [1, 2, 3],
-    ]
+from day08 import (
+    Map,
+    read_map,
+    visible_trees_from_outside,
+)
 
 
 @pytest.fixture
@@ -33,73 +19,9 @@ def sample_map() -> Map:
     ]
 
 
-def test_number_of_visible_trees_asymmetric_map_left(asymmetric) -> None:
-    visible_left = partial(visible_trees_in_line, asymmetric, Direction.LEFT)
-
-    assert visible_left(0) == 1
-    assert visible_left(1) == 1
-    assert visible_left(2) == 1
-
-
-def test_number_of_visible_trees_ring_of_trees_left(ring_of_trees) -> None:
-    visible_left = partial(visible_trees_in_line, ring_of_trees, Direction.LEFT)
-
-    assert visible_left(0) == 2
-    assert visible_left(1) == 1
-    assert visible_left(2) == 3
-
-
-def test_number_of_visible_trees_asymmetric_map_right(asymmetric) -> None:
-    visible_right = partial(visible_trees_in_line, asymmetric, Direction.RIGHT)
-
-    assert visible_right(0) == 1
-    assert visible_right(1) == 1
-    assert visible_right(2) == 1
-
-
-def test_number_of_visible_trees_ring_of_trees_right(ring_of_trees) -> None:
-    visible_right = partial(visible_trees_in_line, ring_of_trees, Direction.RIGHT)
-
-    assert visible_right(0) == 2
-    assert visible_right(1) == 1
-    assert visible_right(2) == 1
-
-
-def test_number_of_visible_trees_asymmetric_map_down(asymmetric) -> None:
-    visible_down = partial(visible_trees_in_line, asymmetric, Direction.DOWN)
-
-    assert visible_down(0) == 0
-    assert visible_down(1) == 1
-    assert visible_down(2) == 0
-    assert visible_down(3) == 1
-
-
-def test_number_of_visible_trees_ring_of_trees_down(ring_of_trees) -> None:
-    visible_down = partial(visible_trees_in_line, ring_of_trees, Direction.DOWN)
-
-    assert visible_down(0) == 2
-    assert visible_down(1) == 1
-    assert visible_down(2) == 3
-
-
-def test_number_of_visible_trees_asymmetric_map_up(asymmetric) -> None:
-    visible_up = partial(visible_trees_in_line, asymmetric, Direction.UP)
-
-    assert visible_up(0) == 0
-    assert visible_up(1) == 1
-    assert visible_up(2) == 0
-    assert visible_up(3) == 1
-
-
-def test_number_of_visible_trees_ring_of_trees_up(ring_of_trees) -> None:
-    visible_up = partial(visible_trees_in_line, ring_of_trees, Direction.UP)
-
-    assert visible_up(0) == 2
-    assert visible_up(1) == 1
-    assert visible_up(2) == 1
-
-
 def test_read_map(sample_map) -> None:
-    map = read_map("inputs/day08_sample.txt")
+    assert read_map("inputs/day08_sample.txt") == sample_map
 
-    assert map == sample_map
+
+def test_solve_part_one(sample_map) -> None:
+    assert visible_trees_from_outside(sample_map) == 21

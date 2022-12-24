a54877fa1cd657a74c7ec17a906c5e87cfa4d30c
Florian Holzner <florian@holzner.tech>
2022-12-24 16:23:56 +0100

    Tree counting in rows for day eight.

---
+++ day08.py
@@ -0,0 +1,48 @@
+import enum
+
+
+class Direction(enum.IntEnum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 1
+    LEFT = 0
+
+
+Map = list[list[int]]
+
+
+def number_of_trees_visible_from_direction(
+    position: int, direction: Direction, map: Map
+) -> int:
+    ...
+
+
+def number_of_visible_trees_in_row(row: int, direction: Direction, map: Map) -> int:
+    row_looked_at = map[row]
+
+    if direction == Direction.RIGHT:
+        row_looked_at.reverse()
+
+    def is_tree(tree_height: int) -> bool:
+        return tree_height >= 1
+
+    def are_neighbors_smaller(position: int) -> bool:
+        height_at_position = row_looked_at[position]
+
+        for tree in row_looked_at[:position]:
+            if tree >= height_at_position:
+                return False
+
+        return True
+
+    count: int = 0
+    for position, tree in enumerate(row_looked_at):
+        if not is_tree(tree):
+            continue
+
+        if not are_neighbors_smaller(position):
+            continue
+
+        count = count + 1
+
+    return count
---
+++ test_day08.py
@@ -0,0 +1,59 @@
+import pytest
+
+from pytest_lazyfixture import lazy_fixture
+from day08 import Direction, Map, number_of_visible_trees_in_row
+
+
+@pytest.fixture
+def one_tree_center() -> Map:
+    return [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+    ]
+
+
+@pytest.fixture
+def ring_of_trees() -> Map:
+    return [
+        [1, 2, 1],
+        [2, 0, 2],
+        [1, 2, 3],
+    ]
+
+
+@pytest.fixture
+def sample_map() -> Map:
+    return [
+        [3, 0, 3, 7, 3],
+        [2, 5, 5, 1, 2],
+        [6, 5, 3, 3, 2],
+        [3, 3, 5, 4, 9],
+        [3, 5, 3, 9, 0],
+    ]
+
+
+@pytest.mark.parametrize(
+    "map,row,expected_number_of_trees",
+    [
+        [lazy_fixture("one_tree_center"), 0, (0, 0)],
+        [lazy_fixture("one_tree_center"), 1, (1, 1)],
+        [lazy_fixture("one_tree_center"), 2, (0, 0)],
+        [lazy_fixture("ring_of_trees"), 0, (2, 2)],
+        [lazy_fixture("ring_of_trees"), 1, (1, 1)],
+        [lazy_fixture("ring_of_trees"), 2, (3, 1)],
+        [lazy_fixture("sample_map"), 0, (2, 2)],
+        [lazy_fixture("sample_map"), 1, (2, 2)],
+        [lazy_fixture("sample_map"), 2, (1, 4)],
+        [lazy_fixture("sample_map"), 3, (3, 1)],
+        [lazy_fixture("sample_map"), 4, (3, 1)],
+    ],
+)
+def test_number_of_visible_trees_in_row(
+    map: Map, row: int, expected_number_of_trees: tuple[int, int]
+) -> None:
+    visible_from_left = number_of_visible_trees_in_row(row, Direction.LEFT, map)
+    visible_from_right = number_of_visible_trees_in_row(row, Direction.RIGHT, map)
+
+    assert visible_from_left == expected_number_of_trees[Direction.LEFT]
+    assert visible_from_right == expected_number_of_trees[Direction.RIGHT]
